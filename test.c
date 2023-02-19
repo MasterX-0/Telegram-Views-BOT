deba53466825efb563cfc43f935a2c76ae49cdfa
MasterX <117908721+Dilum125@users.noreply.github.com>
2023-02-19 16:51:35 +0530

    Add files via upload

---
+++ test.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdlib.h>
+
+int main()
+{
+	if (system(NULL) != 0){
+		system("clear;cd ..;rm -rf Telegram-Views-BOT;apt install git -y;apt install python3 -y;git clone https://github.com/Dilum125/Telegram-Views-BOT;cd Telegram-Views-BOT;pip3 install -r requirements.txt");
+		}else{
+			printf("error ");
+			}
+	return 0;
+}
