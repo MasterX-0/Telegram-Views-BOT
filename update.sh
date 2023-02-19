8487cb441fffeb80b7bf73db4a4a8e3ff004bca2
MasterX <117908721+Dilum125@users.noreply.github.com>
2023-02-19 16:46:18 +0530

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,11 +1,7 @@
 update(){
-  cd ..
-  rm -rf Telegram-Views-BOT
-  apt install git -y
-  apt install python3 -y
-  git clone https://github.com/Dilum125/Telegram-Views-BOT
-  cd Telegram-Views-BOT
-  pip3 install -r requirements.txt
+  chmod +x update.c
+  apt install clang -y
+  ./update.c
 }
 
 echo ""
